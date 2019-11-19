e053fa6afb39f7c2ceb63894815047240705be17
jxuluna <931645246@qq.com>
2019-11-19 10:06:18 +0800

    开始

---
+++ src/components/SideBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <el-scrollbar wrap-class="scrollbar-wrapper" class="sidebar-comtainer">
+    <el-menu default-active="1-6-1" class="el-menu sidebar" unique-opened="true" text-color="#fff" :collapse="isCollapse" >
+    </el-menu>
+    
+    </el-scrollbar>
+</template>
